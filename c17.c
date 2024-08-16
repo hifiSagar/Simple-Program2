c57b38f65d0c905901d55ef6ee546d4595b049dd
SAGAR T <ssagu6661@gmail.com>
2024-08-16 11:02:24 -0400

    Add files via upload

---
+++ c17.c
@@ -0,0 +1,18 @@
+
+/*while loop*/
+
+#include<stdio.h>
+int main(){
+    int i=1;
+       
+    while (i<101)
+  
+    
+    {
+       printf("hello world %d \n",i);
+       i++;
+    }
+    return 0;
+    
+}
+/*want's to print hello world in 100 times*/
